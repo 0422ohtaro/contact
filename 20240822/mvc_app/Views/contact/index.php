ab011524ee7611d75591f00d79d52b8fe86da6e7
0422ohtaro <ohtaro.tsurumaki@gmail.com>
2024-09-03 21:02:45 +0900

    Force remove mvc_app directory from main branch

--- 20240822/mvc_app/Views/contact/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('path/to/smarty/libs/Smarty.class.php');
-session_start();
-$smarty = new Smarty();
-$smarty->template_dir = 'path/to/templates';
-$smarty->compile_dir = 'path/to/templates_c';
-
-$smarty->display('index.tpl');
-?>
